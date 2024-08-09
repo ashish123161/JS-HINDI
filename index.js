dd77f340c068339ef308b34b240941f5c83f2af4
ashish <ashishchaudhary@gmail,com>
2024-08-09 16:46:23 +0530

    first commit in README.md

---
+++ index.js
@@ -0,0 +1,39 @@
+// let num=20;
+// console.log(num)
+// console.log(typeof num)
+// let num2='20';
+// console.log(typeof num2)
+
+// let cname='ashish';
+// let cnameone="ashish";
+// let cnametwo='ashish'
+// console.log(cname,typeof cname)
+
+
+// let s=true;
+// console.log(s,typeof s)
+// console.log(s+6)
+
+// let n=null;
+// console.log(typeof n)
+
+
+
+// let num1;
+// console.log(num1)
+
+// let vid=symbol('123')
+// let cid=symbol('123')
+// console.log(vid=cid)
+
+
+// let bigInt=99999999999n;
+// console.log(bigInt,typeof(bigInt))
+
+
+// let obj={
+//     cname:'ashish',
+//     coursename:'js'
+// }
+
+// console.log(obj,typeof obj)
---
+++ js-oper.js
@@ -0,0 +1,101 @@
+
+
+// let v1=12;
+// let v2=34;
+// console.log(v1*v2)
+
+
+// let v1=12;
+// let v2=34;
+// console.log(v1%v2)
+
+
+// let v1=12;
+// let v2=34;
+// console.log(v1/v2)
+
+
+// let v1=12;
+// let v2=34;
+// console.log(v1-v2)
+
+// if statement 
+
+// let mode ='light';
+//     let color;
+//     if (mode=="dark"){
+//         color="black";
+//     }
+//     if (mode==="light"){
+//         color="white";
+//     }
+
+//     console.log(color)
+
+
+// if else statement--
+// let age=25;
+//   if(age<=23){
+//     console.log("you can vote");
+        
+//     } else{
+//         console.log("you can not vote")
+//     }
+
+
+// else if statemenbt
+// let age=20;
+// if(age<10) {
+//     console.log("junior");
+// } else if (age>60){
+//     console.log("senior");
+// }else{
+//     console.log("middle");
+// }
+// console.log(age)
+
+
+
+// terrnary operators----
+// let score=60;
+//   let grade;
+// if(score>=90 && score<=100){
+//     grade="A";
+//  }else if (score>=70 && score <=67){
+//            grade="B";
+//  }else if (score>=50 && score <=60){
+//            grade="C";
+//  }else if (score>=40 && score <=57){
+//          grade="D";
+//    }else if (score>=0 && score <=47){
+//            grade="E";
+// }
+//  console.log("according to score:", grade)
+
+    // LOOP
+
+    // FOR LOOP
+
+// let sum=0;
+//  for(let i=1;i<=8;i++){
+//     sum=sum+i;
+//  }
+//  console.log("sum=",sum);
+//  console.log("loop has end");
+
+
+// WHILE LOPP
+// let i=2;
+// while(i<80){
+//     console.log("i=",i);
+//     i++;
+// }
+
+// let i=20;
+// do{
+//     console.log("ashish");
+//     i++;
+// }while(i<=10);
+
+
+  
